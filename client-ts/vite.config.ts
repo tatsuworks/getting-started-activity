68192e64a41f626c09b884fb4b3b7d646002af3a
Benson <benson@tatsu.works>
2025-03-06 15:51:08 +0800

    Add allowed hosts in env

--- client-ts/vite.config.ts
+++ client-ts/vite.config.ts
@@ -1,21 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  envDir: '../',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, `${process.cwd()}/..`, '')
+  const allowedHosts = [env.ALLOWED_HOSTS ?? ''];
+  
+  return {
+    plugins: [react()],
+    envDir: '../',
+    server: {
+      allowedHosts,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3001',
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        },
+      },
+      hmr: {
+        clientPort: 443,
       },
     },
-    hmr: {
-      clientPort: 443,
-    },
-  },
+  }
 })
